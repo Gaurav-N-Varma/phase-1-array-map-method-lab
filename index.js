d1cb208e0c331d6b50712b9c1344a18a3bd75268
Gaurav <gaurav.narayan.varma@gmail.com>
2022-08-15 09:59:34 -0400

    commit tree 55eca6a0bb09425726fe0b8d62e93be6f36d79ed

--- index.js
+++ index.js
@@ -12,5 +12,31 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  // modifies each string in the array to convert to title case
+  return tutorials.map((string) => {
+
+    // creates string we want to return
+    let newString = ''
+
+    let i = 0
+
+    // loops through each letter in the string
+    for (const letter of string){
+      // if the letter has a space before it, capitalize it and add
+      if (string[string.indexOf(letter, i) - 1] === ' '){
+        newString = newString + letter.toUpperCase()
+        // if the letter does not have a space before it, lowercase it and add
+      } else {
+        newString = newString + letter
+      }
+      i++
+    }
+
+    // capitalize the first letter
+    newString = newString[0].toUpperCase() + newString.slice(1)
+
+    return newString
+  })
 }
+
+console.log(titleCased())
